294c5770d3745f4aaee9255d99e54d1433e3f1a0
Praise <praisetoyo@gmail.com>
2023-10-24 13:56:06 -0700

    set up database and other dependecies

---
+++ db/connect.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectDB = async (url) => {
+  try {
+    await mongoose.connect(url);
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.log('Error connecting to MongoDB Atlas:', err);
+  }
+};
+
+module.exports = connectDB;
