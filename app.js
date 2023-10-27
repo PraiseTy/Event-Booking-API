31eec050bb40b9b23fc6edd94d811641894b43e6
Praise <praisetoyo@gmail.com>
2023-10-27 07:20:18 -0700

    made changes to necessary files

--- app.js
+++ app.js
@@ -2,18 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./db/connect');
 const logger = require('./logging/logger');
-const artistsRouter = require('./routes/artists');
 const artistRouter = require('./routes/artists');
-const eventsRouter = require('./routes/events');
 const eventRouter = require('./routes/events');
 
 app = express();
 
 app.use(express.json());
 
-app.use('/api/v1', artistsRouter);
 app.use('/api/v1/artist', artistRouter);
-app.use('/api/v1', eventsRouter);
 app.use('/api/v1/event', eventRouter);
 
 const port = process.env.PORT || 3000;
